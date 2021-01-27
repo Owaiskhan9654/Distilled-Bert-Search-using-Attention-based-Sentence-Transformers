fc73faed3b950f65a7c32778d143951484c26094
Owaiskhan9654 <Owaiskhan9654@gmail.com>
2021-01-28 01:50:02 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from ranking import rank
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+@app.route('/search/results', methods=['GET', 'POST'])
+def search_request():
+    search_term = request.form["input"]
+    articlelink,score=rank(search_term)
+    return render_template('results.html', res=articlelink ,score=score)
+
+if __name__ == '__main__':
+    app.run(debug=True)
